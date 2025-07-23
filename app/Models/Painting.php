c31717dbc6498953176c469bbb6e808c29dd67b0
Ramon Rius <rmnrius@gmail.com>
2025-07-23 18:19:36 +0800

    '''

--- app/Models/Painting.php
+++ app/Models/Painting.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Painting extends Model{
+    use HasFactory;
     protected $table = 'artwork_listings'; // Specify the table name if it's not the plural of the model name
 
-    protected $fillable = ['title', 'artist'];
+    protected $fillable = ['title', 'price'];
 
 }
 
--- database/migrations/2025_07_23_095409_create_artists_table.php
+++ database/migrations/2025_07_23_095409_create_artists_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('artwork_listings', function (Blueprint $table) {
+        Schema::create('artists', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(App\Models\Artist::class);
-            $table->string('title');
-            $table->string('price');
+            
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('artwork_listings');
+        Schema::dropIfExists('artists');
     }
 };
