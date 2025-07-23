1bd6c4c9b4dcc7d97b23f3759f217648679d70a5
Ramon Rius <rmnrius@gmail.com>
2025-07-23 17:30:32 +0800

    making new tables and adjustments thru artisan

--- app/Models/Painting.php
+++ app/Models/Painting.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Painting extends Model{
     protected $table = 'artwork_listings'; // Specify the table name if it's not the plural of the model name
+
+    protected $fillable = ['title', 'artist'];
+
 }
 
 
